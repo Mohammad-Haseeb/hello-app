ac19e36b22a8bfeefe9d626ab467daea5f6d870b
Muhaamd-Haseeb <64950545+Muhaamd-Haseeb@users.noreply.github.com>
2020-06-04 12:56:09 +0500

    Initial commit

---
+++ build/precache-manifest.25c0f12247ccdc7e2cc1f81eaab4e66e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ad351aeb96571a6131c0407b2c429e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daf44251469f5f9b3873",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c6829f6f19a8f525a7d",
+    "url": "/static/js/2.c1d33b8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1d33b8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daf44251469f5f9b3873",
+    "url": "/static/js/main.230e84da.chunk.js"
+  },
+  {
+    "revision": "3848bfd6bfc76b609c81",
+    "url": "/static/js/runtime-main.4292b2fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
